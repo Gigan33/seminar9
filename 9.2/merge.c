17ad0ec8a385a24b82ce85ea618eb4f272237a4a
Gigan33 <119124978+Gigan33@users.noreply.github.com>
2023-06-16 18:55:26 +0300

    Add files via upload

---
+++ 9.2/merge.c
@@ -0,0 +1,28 @@
+#include "merge.h"
+#include "merge.h"
+
+void merge(FILE *file1, FILE *file2) {
+    char read1;
+    char read2;
+
+    int j = 1, k = 1;
+
+    while (j == 1 || k == 1) {
+        j = 0;
+        k = 0;
+        if ((read1 = fgetc(file1)) != EOF) {
+            putchar(read1);
+            while ((read1 = fgetc(file1)) != '\n') {
+                putchar(read1);
+            }
+            j = 1;
+        }
+        if ((read2 = fgetc(file2)) != EOF) {
+            putchar(read2);
+            while ((read2 = fgetc(file2)) != '\n') {
+                putchar(read2);
+            }
+            k = 1;
+        }
+    }
+}
---
+++ 9.2/merge.h
@@ -0,0 +1,7 @@
+
+
+#ifndef SEMINAR9_1_MERGE_H
+#define SEMINAR9_1_MERGE_H
+#include <stdio.h>
+void merge(FILE *file1, FILE *file2);
+#endif //SEMINAR9_1_MERGE_H
